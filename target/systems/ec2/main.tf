870cd1edbe236d56e74b0d1524a4ab09936366a4
Engineer Felix <111101906+Engineer-Felix@users.noreply.github.com>
2023-07-11 03:23:14 +0800

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,20 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.7.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "server" {
+    source = ""https://github.com/Engineer-Felix/module-systems/tree/main/target//systems/ec2
+    num = 10
+    intance_type = "t2.medium"
+    ami = "ami-04823729c75214919"
+}
